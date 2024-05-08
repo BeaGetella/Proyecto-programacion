3a32b11560baf5b9fcb1efe4cb8b1d7ccc080d3e
Beatriz <beatrizitagetella@gmail.com>
2024-05-07 22:05:09 -0600

    cuarto commit

--- ProyectoProgramacion/src/main/java/module-info.java
+++ ProyectoProgramacion/src/main/java/module-info.java
@@ -2,6 +2,10 @@ module com.beagetella.proyectoprogramacion {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.base;
+    requires java.desktop;
+    requires javafx.graphics;
+    requires java.base;
 
     opens com.beagetella.proyectoprogramacion to javafx.fxml;
     exports com.beagetella.proyectoprogramacion;
